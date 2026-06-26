05dd0ebbe2681699cc22781560450438589e8747
mclee <myoungcheol.lee@bluebirdcorp.com>
2026-06-26 12:27:36 +0900

    deploy: ippatsu web build (ippatsu@14b955a)

--- service-worker.js
+++ service-worker.js
@@ -2,9 +2,9 @@
 
 // CACHE_NAME 끝의 빌드 해시와 BUILD_ASSETS는 빌드 시 vite-plugin-sw-precache가 주입한다.
 // 개발 중(주입 전)에는 앱 셸만 캐시한다.
-const CACHE_NAME = "ippatsu-shell-mquc907h";
+const CACHE_NAME = "ippatsu-shell-mquddlwj";
 const APP_SHELL = ["/", "/index.html", "/manifest.webmanifest", "/icon.svg"];
-const BUILD_ASSETS = ["/assets/SharedSystems-ujxW8pU1.js","/assets/WebGLRenderer-Bx1PMXhC.js","/assets/WebGPURenderer-B_WCaR6i.js","/assets/browserAll-BIVhzyPh.js","/assets/colorToUniform-CmXW-3p3.js","/assets/index-BNV207xh.js","/assets/index-JIlCUzzD.css","/assets/webworkerAll-CmxhIrVN.js"];
+const BUILD_ASSETS = ["/assets/SharedSystems-T1eHponQ.js","/assets/WebGLRenderer-DmE8e4aE.js","/assets/WebGPURenderer-CToRkpyk.js","/assets/browserAll-DakHdGdj.js","/assets/colorToUniform-CSkhITBw.js","/assets/index-BFIxNOty.css","/assets/index-CnJ6qYPI.js","/assets/webworkerAll-Bo1Wb1kn.js"];
 
 self.addEventListener("install", (event) => {
   // 해시된 JS/CSS 번들까지 프리캐시 → 온라인 방문 없이도 오프라인 콜드스타트가 동작한다.
