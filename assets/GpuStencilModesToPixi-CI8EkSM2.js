dc72fc8f04e01fe42ec1a86d91a6e36046ffa5ca
charlie <find1086@gmail.com>
2026-06-28 03:10:32 +0900

    deploy: ippatsu web build (ippatsu@cd1c382)

--- assets/GpuStencilModesToPixi-CI8EkSM2.js
+++ assets/GpuStencilModesToPixi-CI8EkSM2.js
@@ -1,4 +1,4 @@
-import{w as m,g as l,c as a}from"./index-jCbjHwPY.js";const c={name:"local-uniform-bit",vertex:{header:`
+import{w as m,g as l,c as a}from"./index-wVcSFYqh.js";const c={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
