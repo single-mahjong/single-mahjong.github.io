162b5573a01a98cb69c7437e1eab4c95bde3f5d3
charlie <find1086@gmail.com>
2026-06-27 23:20:01 +0900

    deploy: ippatsu web build (ippatsu@7e881ff)

--- assets/GpuStencilModesToPixi-DbkeyZS4.js
+++ assets/GpuStencilModesToPixi-DbkeyZS4.js
@@ -1,4 +1,4 @@
-import{w as m,g as l,c as a}from"./index-zp-Kabbn.js";const c={name:"local-uniform-bit",vertex:{header:`
+import{w as m,g as l,c as a}from"./index-DODMOtYY.js";const c={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
--- service-worker.js
+++ service-worker.js
@@ -2,9 +2,9 @@
 
 // CACHE_NAME 끝의 빌드 해시와 BUILD_ASSETS는 빌드 시 vite-plugin-sw-precache가 주입한다.
 // 개발 중(주입 전)에는 앱 셸만 캐시한다.
-const CACHE_NAME = "ippatsu-shell-mqweaz4i";
+const CACHE_NAME = "ippatsu-shell-mqwg5e9c";
 const APP_SHELL = ["/", "/index.html", "/manifest.webmanifest", "/icon.svg"];
-const BUILD_ASSETS = ["/assets/BitmapFont-Dhs8rQiO.js","/assets/CanvasRenderer-BBDDIuyn.js","/assets/Filter-DBp7NbPN.js","/assets/GpuStencilModesToPixi-hXGHmOn7.js","/assets/RenderTargetSystem-OHGEtf7n.js","/assets/WebGLRenderer-JmjvVwUC.js","/assets/WebGPURenderer-v5db1U2_.js","/assets/browserAll-8gCssGJx.js","/assets/index-DUiy7vM6.css","/assets/index-zp-Kabbn.js","/assets/webworkerAll-DI5edG1T.js"];
+const BUILD_ASSETS = ["/assets/BitmapFont-DOUtMEAB.js","/assets/CanvasRenderer-MUfbhfbS.js","/assets/Filter-DKjNLgt_.js","/assets/GpuStencilModesToPixi-DbkeyZS4.js","/assets/RenderTargetSystem-ByfD6Pzb.js","/assets/WebGLRenderer-B3zDiEML.js","/assets/WebGPURenderer-vFwc25Q6.js","/assets/browserAll-Ddb6uwaq.js","/assets/index-DODMOtYY.js","/assets/index-XLHCCaXp.css","/assets/webworkerAll-BBl_xbVE.js"];
 
 self.addEventListener("install", (event) => {
   // 해시된 JS/CSS 번들까지 프리캐시 → 온라인 방문 없이도 오프라인 콜드스타트가 동작한다.
